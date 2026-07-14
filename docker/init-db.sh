1078007ade3b5707f946a492456d564a0ca7fbb2
FoolByte <khairuliman738@gmail.com>
2026-07-15 03:44:17 +0700

    chore(docker): add .dockerignore files and parameterize metabase password
    - Add .dockerignore at repo root and apps/landing/ to speed up builds
    - Make init-db.sh metabase_ro password configurable via METABASE_RO_PASSWORD env var
    
    Phase 9 was already completed in a prior session.

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -15,7 +15,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     DO \$\$
     BEGIN
         IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'metabase_ro') THEN
-            CREATE ROLE metabase_ro WITH LOGIN PASSWORD 'metabase_ro_pass';
+            CREATE ROLE metabase_ro WITH LOGIN PASSWORD '${METABASE_RO_PASSWORD:-metabase_ro_pass}';
         END IF;
     END
     \$\$;
